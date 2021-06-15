364cb9762b7db5c628cc5a8450ef2efa28730eb0
Jonathan SAHM <pro@jsahm.fr>
2021-06-15 08:35:57 +0200

    Add exception translation

--- src/EventSubscriber/AuthenticationFailureListener.php
+++ src/EventSubscriber/AuthenticationFailureListener.php
@@ -6,7 +6,10 @@ namespace PhpGuild\ApiBundle\EventSubscriber;
 
 use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationFailureEvent;
 use PhpGuild\ApiBundle\Http\RequestHandler;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
 use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
  * Class AuthenticationFailureListener
@@ -16,14 +19,19 @@ class AuthenticationFailureListener
     /** @var RequestHandler $requestHandler */
     private $requestHandler;
 
+    /** @var TranslatorInterface $translator */
+    private $translator;
+
     /**
      * AuthenticationFailureListener constructor.
      *
-     * @param RequestHandler $requestHandler
+     * @param RequestHandler      $requestHandler
+     * @param TranslatorInterface $translator
      */
-    public function __construct(RequestHandler $requestHandler)
+    public function __construct(RequestHandler $requestHandler, TranslatorInterface $translator)
     {
         $this->requestHandler = $requestHandler;
+        $this->translator = $translator;
     }
 
     /**
@@ -35,8 +43,17 @@ class AuthenticationFailureListener
      */
     public function onAuthenticationFailureResponse(AuthenticationFailureEvent $event): void
     {
+        $exception = $event->getException();
+        if ($exception instanceof BadCredentialsException) {
+            $exception = new BadCredentialsException(
+                $this->translator->trans($exception->getMessage(), [], 'security'),
+                $exception->getCode(),
+                $exception
+            );
+        }
+
         $event->setResponse(
-            $this->requestHandler->getResponse($event->getException(), 400)
+            $this->requestHandler->getResponse($exception, Response::HTTP_UNAUTHORIZED)
         );
     }
 }
